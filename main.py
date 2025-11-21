abbd1aa174aa00a677b4a389caa07712f447d6af
grushalj65-svg <grushalj65@gmail.com>
2025-11-21 23:10:33 +0200

    2

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import logging
-from datetime import datetime
 
+logging.basicConfig(level=logging.ERROR, format='%(levelname)s: %(message)s')
 
-logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')          # Налаштування логування
-
-
-now = datetime.now()          # Отримуємо поточну дату
-year = now.year
-month = now.month
-day = now.day
-
-
-logging.info("Сьогоднішня дата: {}-{}-{}".format(year, month, day))          # Виводимо повідомлення через logging
+try:
+    x = 10 / 0
+except Exception as e:
+    logging.error("Виникла помилка: " + str(e))
